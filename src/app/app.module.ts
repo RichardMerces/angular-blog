f10219c9d50988cf2fd5ef42f87326e90bc4e676
Richard Merces <richardmerces@outlook.com>
2023-11-29 20:48:59 -0300

    created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { MenuOptionsComponent } from './components/menu-options/menu-options.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    MenuTitleComponent,
+    MenuOptionsComponent,
+    SmallCardComponent
   ],
   imports: [
     BrowserModule,
